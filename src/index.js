6ba0a11a2bb85452f22a3144aa4c4293f295f544
Kanikabhatia08 <kanikabhatia08sep@gmail.com>
2024-11-15 18:52:33 +0530

    scroll

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ParallaxProvider } from 'react-scroll-parallax';
+import ScrollToTop from './components/ScrollToTop.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ParallaxProvider>
+      <ScrollToTop />
         <App />
       </ParallaxProvider>
     </BrowserRouter>
